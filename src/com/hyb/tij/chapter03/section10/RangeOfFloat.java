90fce7645ec17e684191cff036e0b2c07f788bbc
muzihuoyubai <muzihuoyubai@163.com>
2018-01-31 13:54:58 +0800

    init

---
+++ src/com/hyb/tij/chapter03/section10/RangeOfFloat.java
@@ -0,0 +1,12 @@
+package com.hyb.tij.chapter03.section10;
+
+public class RangeOfFloat {
+
+	public static void main(String[] args) {
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.MIN_VALUE);
+	}
+
+}
