f3ff6f925178add5c3bef9b349f65bc0294f2617
muzihuoyubai <muzihuoyubai@163.com>
2018-02-04 23:39:57 +0800

    移除警告提示

--- src/com/hyb/tij/chapter05/section08/InitArray.java
+++ src/com/hyb/tij/chapter05/section08/InitArray.java
@@ -1,7 +1,7 @@
 package com.hyb.tij.chapter05.section08;
 
 public class InitArray {
-	
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		int [] test = {1,2,3,4};
 		Object[] test1;
