e926ef9b35ab6df3c36e5452c4ff0e8b2bf6a450
Liyi <Liyi@ly>
2018-02-05 19:40:17 +0800

    无法继承私有化构造器的父类

---
+++ src/com/hyb/tij/chapter07/section04/Father.java
@@ -0,0 +1,9 @@
+package com.hyb.tij.chapter07.section04;
+
+public class Father {
+	
+	private Father(){
+		
+	}
+	
+}
---
+++ src/com/hyb/tij/chapter07/section04/Sun.java
@@ -0,0 +1,17 @@
+package com.hyb.tij.chapter07.section04;
+
+/**
+ * @description 不能创建该类型，因为没有可见的父类构造器
+ * @author Liyi
+ * @date 2018年2月5日
+ */
+//public class Sun extends Father {
+//	
+//	Sun(String test){
+//		
+//	}
+//
+//	public static void main(String[] args) {
+//
+//	}
+//}
