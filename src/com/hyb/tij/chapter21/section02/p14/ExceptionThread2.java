f8efb79f775e44d39589c199a6f955d8d201fd2e
muzihuoyubai <muzihuoyubai@163.com>
2018-03-27 20:08:29 +0800

    解除注释

--- src/com/hyb/tij/chapter21/section02/p14/ExceptionThread2.java
+++ src/com/hyb/tij/chapter21/section02/p14/ExceptionThread2.java
@@ -7,6 +7,6 @@ public class ExceptionThread2 implements Runnable {
 		Thread t = Thread.currentThread();
 		System.out.println("run() by " + t);
 		System.out.println("eh = " + t.getUncaughtExceptionHandler());
-//		throw new RuntimeException();
+		throw new RuntimeException();
 	}
 }
